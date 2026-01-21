af9497edf7d0bef3d1e11a6d8f98a12a3a997770
Luka Sukovic <sukovicluka1910@gmail.com>
2026-01-21 19:26:59 +0100

    SCRUM-43: Implementirana sadnja vinove loze

---
+++ Domain/Servisi/IVinovaLozaServis.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Modeli;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Servisi
+{
+    public interface IVinovaLozaServis
+    {
+        VinovaLoza ZasadiLozu(string naziv);
+        VinovaLoza ZasadiLozu(
+           string naziv,
+           double nivoSeceraBrix,
+           int godinaSadnje,
+           string region
+       );
+    }
+}
---
+++ Services/VinogradServisi/VinovaLozaServis.cs
@@ -0,0 +1,46 @@
+﻿using Domain.Modeli;
+using Domain.Repozitorijumi;
+using Domain.Servisi;
+using Domain.Enumeracije;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.VinogradServisi
+{
+    public class VinovaLozaServis : IVinovaLozaServis
+    {
+        private readonly IVinovaLozaRepozitorijum _vinovaLozaRepo;
+        private readonly Random _random = new Random();
+
+        public VinovaLozaServis(IVinovaLozaRepozitorijum vinovaLozaRepo)
+        {
+            _vinovaLozaRepo = vinovaLozaRepo;
+        }
+
+        public VinovaLoza ZasadiLozu(string naziv)
+        {
+            double brix = Math.Round(15.0 + _random.NextDouble() * (20.8 - 15.0), 2);
+            int godinaSadnje = DateTime.Now.Year;
+            string region = "Toskana";
+
+            return ZasadiLozu(naziv, brix, godinaSadnje, region);
+        }
+
+        public VinovaLoza ZasadiLozu(string naziv, double nivoSeceraBrix, int godinaSadnje, string region)
+        {
+            var novaLoza = new VinovaLoza(
+                Guid.NewGuid(),
+                naziv,
+                nivoSeceraBrix,
+                godinaSadnje,
+                region,
+                FazaZrelostiLoze.Posadjena
+            );
+
+            return _vinovaLozaRepo.DodajVinovuLozu(novaLoza);
+        }
+    }
+}
